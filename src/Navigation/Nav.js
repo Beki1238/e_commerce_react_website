c34bef1f3227129b82c75bc0612d14a17bd9bee3
beki1238 <bahibeki@gmail.com>
2025-02-21 08:19:50 +0300

    e commerce

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import {FiHeart} from 'react-icons/fi'
-import {AiPOutlineShoppingCart,AiOutlineUserAdd, AiOutlineShoppingCart} from 'react-icons/ai'
-import './Nav.css'
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import "./Nav.css";
 
-const Nav = () => {
+const Nav = ({ handleInputChange, query }) => {
   return (
     <nav>
-        <div className="nav-container">
-            <input className='search-input'  type='text' placeholder='enter your search shoes'/>
-        </div>
-
-    <div className="profile-container">
-        <a href='#'>
-            <FiHeart className='nav-icons'/>
+      <div className="nav-container">
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
         </a>
-        <a href='#'>
-            <AiOutlineShoppingCart className='nav-icons'/>
+        <a href="">
+          <AiOutlineShoppingCart className="nav-icons" />
         </a>
-        <a href='#'>
-            <AiOutlineUserAdd className='nav-icons'/>
+        <a href="">
+          <AiOutlineUserAdd className="nav-icons" />
         </a>
-            </div>    
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
